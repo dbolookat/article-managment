8ec8f156e18ed1fcfc1af77321aaf788e05bef75
danial <d.bolookat@gmail.com>
2021-07-20 20:55:08 +0430

    frist commit

--- src/ir/maktab56/hw7/repository/UserRepository.java
+++ src/ir/maktab56/hw7/repository/UserRepository.java
@@ -3,6 +3,7 @@ package ir.maktab56.hw7.repository;
 public class UserRepository implements BasseRepository {
     @Override
     public void delete() {
+        System.out.println("delete");
 
     }
 
